052410e035465ed543b154647d83b8b16d395929
David Cnoops <davidcnoops@outlook.com>
2024-11-13 16:30:23 +0100

    first xata query

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div className="">Feedbackland</div>;
+import { getXataClient, DatabaseSchema } from "@/app/db/xata";
+import { Kysely } from "kysely";
+import { XataDialect, Model } from "@xata.io/kysely";
+
+const xata = getXataClient();
+
+const db = new Kysely<Model<DatabaseSchema>>({
+  dialect: new XataDialect({ xata }),
+});
+
+export default async function Home() {
+  const record = await db
+    .selectFrom("posts")
+    .selectAll()
+    .where("xata_id", "=", "rec_csqc9bsgfdtt6fchk070")
+    .executeTakeFirst();
+
+  console.log(record);
+
+  return <div className="">{record?.text}</div>;
 }
