e5adf530ca141993929c3e389dfcb54c71b9d97b
David Cnoops <davidcnoops@outlook.com>
2024-11-13 21:05:00 +0100

    remove commented out xata connection code

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,18 +1,7 @@
 import "server-only";
-
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 import { DB } from "@/app/db/schema";
-
-// import { getXataClient, DatabaseSchema } from "@/app/db/xata";
-// import { Kysely } from "kysely";
-// import { XataDialect, Model } from "@xata.io/kysely";
-
-// const xata = getXataClient();
-
-// const db = new Kysely<Model<DatabaseSchema>>({
-//   dialect: new XataDialect({ xata }),
-// });
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
