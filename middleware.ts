88afc8a447a21e1fc83ca6fff6ff7a495d7e93ba
David Cnoops <davidcnoops@outlook.com>
2024-12-20 15:10:34 +0100

    better subdomain capture logic

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,12 @@ export function middleware(request: NextRequest) {
   const hostname = request.headers.get("host");
   const subdomain = hostname?.split(".")?.[0];
   const response = NextResponse.next();
-  response.headers.set("x-subdomain", subdomain || "");
+
+  if (subdomain && subdomain !== "www" && !subdomain.includes("localhost")) {
+    response.headers.set("x-subdomain", subdomain);
+  } else {
+    response.headers.set("x-subdomain", "");
+  }
+
   return response;
 }
