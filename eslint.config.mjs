887606a771247586622f10ac5dba492e9a4b0c7d
David Cnoops <davidcnoops@outlook.com>
2024-12-27 16:56:47 +0100

    linting rules error to warn change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
   }),
 ];
 
