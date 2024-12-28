2f0afdc9f05b6071d97cade24bdd775a0070e661
David Cnoops <davidcnoops@outlook.com>
2024-12-28 22:39:57 +0100

    deploy test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import "@iframe-resizer/child";
 import { getSession } from "@/app/utils/server/helpers";
-import { SignOutButton } from "./components/sign-out-button";
-import { SignInDialog } from "./components/sign-in-dialog";
+// import { SignOutButton } from "./components/sign-out-button";
+// import { SignInDialog } from "./components/sign-in-dialog";
 
 export default async function Home() {
   const session = await getSession();
@@ -10,14 +10,14 @@ export default async function Home() {
     <div className="flex flex-col space-y-3">
       <h1>Feedbackland WIP</h1>
       {session && <div>Session username: {session?.user?.name}</div>}
-      <div className="">
+      {/* <div className="">
         <div>
           <SignOutButton />
         </div>
         <div>
           <SignInDialog />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
