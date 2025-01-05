bef3a557b0d19fb643b9a9a3d40e24d1795942ca
David Cnoops <davidcnoops@outlook.com>
2025-01-05 21:17:06 +0100

    remove unusted queries

--- app/queries/index.ts
+++ app/queries/index.ts
@@ -14,12 +14,6 @@ export const createOrg = async ({
   }
 
   return await db.transaction().execute(async (trx) => {
-    // const user = await trx
-    //   .selectFrom("user")
-    //   .select("id")
-    //   .where("user.id", "=", userId)
-    //   .executeTakeFirstOrThrow();
-
     const org = await trx
       .insertInto("org")
       .values({
@@ -48,34 +42,3 @@ export const getOrg = async ({ subdomain }: { subdomain: string }) => {
     .select(["org.id", "org.name", "org.subdomain"])
     .executeTakeFirst();
 };
-
-// export const getOrgs = async ({ userId }: { userId: string }) => {
-//   const orgs = await db
-//     .selectFrom("user_org")
-//     .innerJoin("org", "user_org.org_id", "org.id") // Join with the org table
-//     .where("user_org.user_id", "=", userId) // Filter by the user's ID
-//     .select([
-//       "org.id", // Select organization ID
-//       "org.name", // Select organization name (adjust column names as needed)
-//       "org.subdomain", // Select subdomain, if applicable
-//     ])
-//     .execute();
-//   return orgs;
-// };
-
-// export const getUserWithOrgAndRole = async ({ userId }: { userId: string }) => {
-//   const result = await db
-//     .selectFrom("user")
-//     .innerJoin("user_org", "user.id", "user_org.user_id")
-//     .innerJoin("org", "user_org.org_id", "org.id")
-//     .select([
-//       "user.id as user_id",
-//       "user.name as user_name",
-//       "org.id as org_id",
-//       "org.name as org_name",
-//       "user_org.role as user_org_role",
-//     ])
-//     .where("user.id", "=", userId)
-//     .executeTakeFirst();
-//   return result;
-// };
