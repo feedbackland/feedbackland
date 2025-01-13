856c6b01ed8e82e93f41d75354dd36a1b15544f0
David Cnoops <davidcnoops@gmail.com>
2025-01-13 17:26:56 +0100

    code block wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import "@iframe-resizer/child";
 import { getSession } from "@/app/utils/server/helpers";
 import { SignOutButton } from "@/app/_components/sign-out/sign-out-button";
 import { SignInDialog } from "@/app/_components/sign-in/sign-in-dialog";
+import { Code } from "@/components/ui/code";
 
 export default async function RootPage() {
   const session = await getSession();
 
   return (
     <div className="flex items-center space-x-5">
+      <div>
+        <Code code="`const numbers = [1, 2, 3]{:ts}`" />
+      </div>
       {session ? <SignOutButton /> : <SignInDialog />}
     </div>
   );
