0ed76b228aadbb53bff2238a95f6b37213c37791
David Cnoops <davidcnoops@outlook.com>
2025-02-25 13:36:18 +0100

    firebase auth test

--- components/app/sso/sso-button-google.tsx
+++ components/app/sso/sso-button-google.tsx
@@ -4,13 +4,25 @@ import { signIn } from "@/lib/client/auth-client";
 import { Button } from "@/components/ui/button";
 import { GoogleLogo } from "@/components/ui/logos";
 import { getSSOCallbackUrl } from "@/lib/client/utils";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "@/lib/firebase";
 
 export function SSOButtonGoogle({ context }: { context?: string }) {
+  const provider = new GoogleAuthProvider();
+
   const continueWithGoogle = async () => {
-    await signIn.social({
-      provider: "google",
-      callbackURL: getSSOCallbackUrl({ context }),
-    });
+    // await signIn.social({
+    //   provider: "google",
+    //   callbackURL: getSSOCallbackUrl({ context }),
+    // });
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log("success", result);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
   };
 
   return (
