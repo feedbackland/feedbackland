a74cda6195ec6ee29fdc73825c7c4a2280784e4b
David Cnoops <davidcnoops@outlook.com>
2025-02-27 21:36:45 +0100

    firebase private key fix

--- lib/auth/admin.ts
+++ lib/auth/admin.ts
@@ -7,7 +7,9 @@ const adminApp =
         credential: cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.split(
+            String.raw`\n`,
+          ).join("\n"),
         }),
       })
     : getApps()[0];
