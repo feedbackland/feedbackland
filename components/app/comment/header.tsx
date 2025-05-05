48494b3494be3d44a85cfc3ef3bd15b486f91c40
David Cnoops <davidcnoops@outlook.com>
2025-05-05 10:21:55 +0200

    admin badge

--- components/app/comment/header.tsx
+++ components/app/comment/header.tsx
@@ -3,6 +3,8 @@
 import { timeAgo } from "@/lib/time-ago";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAuth } from "@/hooks/use-auth";
+import { Badge } from "@/components/ui/badge";
 
 export function CommentHeader({
   authorPhotoURL,
@@ -15,6 +17,9 @@ export function CommentHeader({
   createdAt: Date;
   className?: React.ComponentProps<"div">["className"];
 }) {
+  const { session } = useAuth();
+  const isAdmin = session?.userOrg?.role === "admin";
+
   return (
     <div className={cn("flex items-center gap-1", className)}>
       <Avatar className="-ml-1 scale-80!">
@@ -22,7 +27,14 @@ export function CommentHeader({
         <AvatarFallback>{authorName?.charAt(0)}</AvatarFallback>
       </Avatar>
       <div className="text-muted-foreground flex items-center gap-1 text-xs font-normal">
-        <div className="text-primary">{authorName}</div>
+        <div className="text-primary">
+          {authorName}
+          {isAdmin && (
+            <Badge variant="default" className="ml-1 scale-85">
+              Admin
+            </Badge>
+          )}
+        </div>
         <span className="text-[8px]">•</span>
         <div className="">{timeAgo.format(createdAt, "round")}</div>
       </div>
