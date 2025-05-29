e3c0c127a7b4559708f2e7133c7d8775972133bc
David Cnoops <davidcnoops@outlook.com>
2025-05-29 16:51:00 +0200

    optimized setAllActivitiesSeenQuery

--- queries/set-all-activities-seen.ts
+++ queries/set-all-activities-seen.ts
@@ -11,87 +11,25 @@ export const setAllActivitiesSeenQuery = async ({
 }) => {
   try {
     await db
-      .with("all_items", (db) =>
-        db
-          .selectFrom("feedback")
-          .where("orgId", "=", orgId)
-          .select(["feedback.id"])
-          .union(
-            db
-              .selectFrom("comment")
-              .innerJoin("feedback", "comment.postId", "feedback.id")
-              .where("feedback.orgId", "=", orgId)
-              .select(["comment.id"]),
-          ),
-      )
       .insertInto("activity_seen")
       .columns(["userId", "itemId"])
       .expression((eb) =>
         eb
-          .selectFrom("all_items")
-          .select([eb.val(userId).as("userId"), "all_items.id as itemId"]),
+          .selectFrom("feedback")
+          .where("orgId", "=", orgId)
+          .select([eb.val(userId).as("userId"), "feedback.id as itemId"])
+          .unionAll(
+            eb
+              .selectFrom("comment")
+              .innerJoin("feedback", "comment.postId", "feedback.id")
+              .where("feedback.orgId", "=", orgId)
+              .select([eb.val(userId).as("userId"), "comment.id as itemId"]),
+          ),
       )
       .onConflict((oc) => oc.columns(["userId", "itemId"]).doNothing())
       .execute();
 
     return true;
-
-    // // Insert feedback IDs into activity_seen
-    // await db
-    //   .insertInto("activity_seen")
-    //   .columns(["userId", "itemId"])
-    //   .expression((eb) =>
-    //     eb
-    //       .selectFrom("feedback")
-    //       .select((subEb) => [
-    //         subEb.val(userId).as("userId"),
-    //         "feedback.id as itemId",
-    //       ])
-    //       .where("feedback.orgId", "=", orgId)
-    //       .where((eb) =>
-    //         eb.not(
-    //           eb.exists(
-    //             db
-    //               .selectFrom("activity_seen as seen")
-    //               .select("seen.itemId")
-    //               .whereRef("seen.itemId", "=", eb.ref("feedback.id"))
-    //               .where("seen.userId", "=", userId),
-    //           ),
-    //         ),
-    //       ),
-    //   )
-    //   .onConflict((oc) => oc.columns(["userId", "itemId"]).doNothing())
-    //   .execute();
-
-    // // Insert comment IDs into activity_seen
-    // await db
-    //   .insertInto("activity_seen")
-    //   .columns(["userId", "itemId"])
-    //   .expression((eb) =>
-    //     eb
-    //       .selectFrom("comment")
-    //       .innerJoin("feedback", "comment.postId", "feedback.id")
-    //       .select((subEb) => [
-    //         subEb.val(userId).as("userId"),
-    //         "comment.id as itemId",
-    //       ])
-    //       .where("feedback.orgId", "=", orgId)
-    //       .where((eb) =>
-    //         eb.not(
-    //           eb.exists(
-    //             db
-    //               .selectFrom("activity_seen as seen")
-    //               .select("seen.itemId")
-    //               .whereRef("seen.itemId", "=", eb.ref("comment.id"))
-    //               .where("seen.userId", "=", userId),
-    //           ),
-    //         ),
-    //       ),
-    //   )
-    //   .onConflict((oc) => oc.columns(["userId", "itemId"]).doNothing())
-    //   .execute();
-
-    // return true;
   } catch (error) {
     throw error;
   }
