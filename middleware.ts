47729bdc083d5fb74c218bac515aaaa23ab621d9
David Cnoops <davidcnoops@outlook.com>
2025-05-30 11:11:41 +0200

    remove middleware uuid redirect

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { validate as uuidValidate } from "uuid";
-import { version as uuidVersion } from "uuid";
-import { Org } from "@/db/schema";
-import { getMaindomain, getSubdomain } from "@/lib/utils";
+import { getSubdomain } from "@/lib/utils";
 
 export const config = {
   matcher: [
@@ -17,63 +14,18 @@ export const config = {
   ],
 };
 
-const isUUID = (uuid: string) => {
-  return uuidValidate(uuid) && uuidVersion(uuid) === 4;
-};
-
-const upsertOrg = async ({ orgId }: { orgId: string }) => {
-  try {
-    const response = await fetch(
-      `${process.env.API_BASE_URL}/api/org/upsert-org`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ orgId }),
-      },
-    );
-
-    const org: Org = await response.json();
-
-    return org;
-  } catch (error) {
-    console.error("Error upserting org:", error);
-    throw error;
-  }
-};
-
 export async function middleware(req: NextRequest) {
   let response = NextResponse.next();
   const url = req.nextUrl.clone();
   const host = req.headers.get("host");
-  const { pathname, search, origin, protocol } = url;
+  const { pathname, search } = url;
   const isLocalhost = host?.includes("localhost");
   const urlString = url.toString();
   const subdomain = getSubdomain(urlString);
-  const mainDomain = getMaindomain(urlString);
 
-  if (subdomain && subdomain.length > 0) {
-    const isUUIDSubdomain = isUUID(subdomain);
-
-    if (isUUIDSubdomain) {
-      const orgId = subdomain;
-
-      const { orgSubdomain } = await upsertOrg({
-        orgId,
-      });
-
-      const redirectUrl = isLocalhost
-        ? `${origin}/${orgSubdomain}${search}`
-        : `${protocol}//${orgSubdomain}.${mainDomain}${search}`;
-
-      response = NextResponse.redirect(redirectUrl);
-    }
-
-    if (!isUUIDSubdomain && !isLocalhost) {
-      const newUrl = `/${subdomain}${pathname}${search}`;
-      response = NextResponse.rewrite(new URL(newUrl, req.url));
-    }
+  if (!isLocalhost) {
+    const newUrl = `/${subdomain}${pathname}${search}`;
+    response = NextResponse.rewrite(new URL(newUrl, req.url));
   }
 
   return response;
