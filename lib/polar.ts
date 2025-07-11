3fc1dd32b5a62d5b5ad62cdcd9673e9461f8d5a7
David Cnoops <davidcnoops@outlook.com>
2025-07-11 17:40:04 +0200

    polar config

--- lib/polar.ts
+++ lib/polar.ts
@@ -2,5 +2,5 @@ import { Polar } from "@polar-sh/sdk";
 
 export const polar = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN!,
-  server: "sandbox", // Use this option if you're using the sandbox environment - else use 'production' or omit the parameter
+  server: process.env.POLAR_SERVER as "production" | "sandbox",
 });
