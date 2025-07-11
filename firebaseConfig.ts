3403dd8f7e65a3124c5b38e6a902ca77f292231b
David Cnoops <davidcnoops@outlook.com>
2025-07-11 17:13:27 +0200

    firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBUh1KIHvkuI1YB7GNXphi8q2oG3JQIpqM",
+  authDomain: "feedbackland-5bf9e.firebaseapp.com",
+  projectId: "feedbackland-5bf9e",
+  storageBucket: "feedbackland-5bf9e.firebasestorage.app",
+  messagingSenderId: "181921772665",
+  appId: "1:181921772665:web:3ed1790346b67060ef10a8",
+};
