5e2c2d9abe7e0def39fc8cb8ffd2bcb6f0b508ea
David Cnoops <davidcnoops@outlook.com>
2025-08-18 11:58:00 +0200

    upsert org logic

--- queries/upsert-org.ts
+++ queries/upsert-org.ts
@@ -1,27 +1,74 @@
 "server-only";
 
 import { db } from "@/db/db";
-import { createOrgQuery } from "./create-org";
+import { v4 as uuidv4 } from "uuid";
+import {
+  uniqueNamesGenerator,
+  colors,
+  animals,
+  NumberDictionary,
+} from "unique-names-generator";
 
 export const upsertOrgQuery = async ({ orgId }: { orgId: string }) => {
   try {
-    let org = await db
-      .selectFrom("org")
-      .select(["orgSubdomain"])
-      .where("id", "=", orgId)
-      .executeTakeFirst();
+    return await db.transaction().execute(async (trx) => {
+      let subdomain: string | undefined;
 
-    if (!org) {
-      org = await createOrgQuery({ orgId });
-    }
+      const org = await trx
+        .selectFrom("org")
+        .select(["orgSubdomain"])
+        .where("id", "=", orgId)
+        .executeTakeFirst();
 
-    const subdomain = org?.orgSubdomain;
+      subdomain = org?.orgSubdomain;
 
-    if (subdomain) {
-      return subdomain;
-    } else {
-      throw new Error("No subdomain found");
-    }
+      if (!org) {
+        const id = uuidv4();
+
+        const orgSubdomain = uniqueNamesGenerator({
+          dictionaries: [
+            colors,
+            animals,
+            NumberDictionary.generate({
+              min: 100,
+              max: 999,
+            }),
+          ],
+          length: 3,
+          separator: "-",
+        });
+
+        const newOrg = await trx
+          .insertInto("org")
+          .values({
+            id,
+            orgSubdomain,
+          })
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        subdomain = newOrg.orgSubdomain;
+
+        await trx
+          .insertInto("feedback")
+          .values({
+            title: "An example feedback post",
+            description:
+              "Just an example of a feedback post. Feel free to delete it.",
+            category: "general feedback",
+            authorId: null,
+            orgId: newOrg.id,
+            embedding: null,
+          })
+          .execute();
+      }
+
+      if (subdomain) {
+        return subdomain;
+      } else {
+        throw new Error("No subdomain found");
+      }
+    });
   } catch (error) {
     throw error;
   }
