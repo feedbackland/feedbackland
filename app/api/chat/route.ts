fb8cc126e84a1cfac688f3127fef7e937767b874
David Cnoops <davidcnoops@outlook.com>
2025-10-06 16:05:24 +0200

    use open router for ask ai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,12 @@
 import { getAllActiveFeedbackPosts } from "@/queries/get-all-active-feedback-posts";
-import { google } from "@ai-sdk/google";
 import { convertToModelMessages, streamText } from "ai";
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 
 export const maxDuration = 30;
+
+const openrouter = createOpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
 
 export async function POST(req: Request) {
   const input = await req.json();
@@ -14,7 +18,7 @@ export async function POST(req: Request) {
   });
 
   const result = streamText({
-    model: google("gemini-2.5-flash-lite-preview-09-2025"),
+    model: openrouter("google/gemini-2.5-flash-lite-preview-09-2025"),
     messages: convertToModelMessages(messages),
     system: `You are a helpful assistant with access to the following feedback posts data:
 
