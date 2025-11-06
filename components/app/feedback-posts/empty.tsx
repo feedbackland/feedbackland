8a8e7048bb35f1ffbb67f670b8b0e0d22d77e0bd
David Cnoops <davidcnoops@outlook.com>
2025-11-06 16:33:38 +0100

    feedback posts empty state copy

--- components/app/feedback-posts/empty.tsx
+++ components/app/feedback-posts/empty.tsx
@@ -12,15 +12,14 @@ import { TextSelect } from "lucide-react";
 
 export function FeedbackPostsEmpty() {
   return (
-    <Empty className="md:border-2 md:border-dashed md:py-[117px]!">
+    <Empty className="md:border-2 md:border-dashed md:py-[127px]!">
       <EmptyHeader>
         <EmptyMedia variant="icon">
           <TextSelect />
         </EmptyMedia>
         <EmptyTitle>No Feedback Yet</EmptyTitle>
         <EmptyDescription>
-          No feedback has been submitted yet. Be the first to share your
-          thoughts!
+          Be the first to share your thoughts!
         </EmptyDescription>
       </EmptyHeader>
     </Empty>
